29d7548ebe00c1999899f4b05dbd33795767a7ce
Rasmus Bååth <rasmus.baath@gmail.com>
2017-11-08 11:50:48 +0100

    Added the narrative part of the notebook

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 * Project admin page: https://www.datacamp.com/teach/repositories/1808/branches/master
 * Project on datacamp.com: https://projects.datacamp.com/projects/33
+
+The global_temperature.csv dataset is from here: http://berkeleyearth.org/data/ .
+
+Top 3 smart phone companies is from here: https://en.wikipedia.org/wiki/List_of_best-selling_mobile_phones#2016_2
+
+http://python-visualization.github.io/folium/
--- requirements.sh
+++ requirements.sh
@@ -2,7 +2,21 @@
 set -e
 
 ##### Install specific package versions with pip #####
+pip3 install pandas==0.20.3
+pip3 install folium==0.5.0
+pip3 install matplotlib==2.0.2
 
-# pip3 install pandas==0.19.1
-# pip3 install tweepy==3.5.0
-# pip3 install bs4==4.4.1
+##### Add files to the datasets/ folder on the live project #####
+
+# A list of files in datasets/ separated by space, needs to be manually updated
+FILES=""
+# The URL where the assets of the project lives
+ASSETURL=
+# Where the datasets will be compied on the Project image.
+DATADIR=/home/repl/datasets
+  
+mkdir -p $DATADIR
+for FILE in $FILES
+do
+	wget -O $DATADIR/$FILE $ASSETURL/$FILE
+done
