7a19af9e53274ae40173ac0ef90c98ad5f4618fd
Rasmus Bååth <rasmus.baath@gmail.com>
2017-11-08 17:00:28 +0100

    Fixed path

--- requirements.sh
+++ requirements.sh
@@ -11,7 +11,7 @@ pip3 install matplotlib==2.0.2
 # A list of files in datasets/ separated by space, needs to be manually updated
 FILES="global_temperature.csv run_code_cell_image.png"
 # The URL where the assets of the project lives
-ASSETURL=https://s3.amazonaws.com/assets.datacamp.com/production/project_33
+ASSETURL=https://s3.amazonaws.com/assets.datacamp.com/production/project_33/datasets
 # Where the datasets will be compied on the Project image.
 DATADIR=/home/repl/datasets
   
