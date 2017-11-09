bf20ae3bbd6263c3bb7c50584be0a3430bac5857
Rasmus Bååth <rasmus.baath@gmail.com>
2017-11-09 12:08:35 +0100

    Added project image.

--- requirements.sh
+++ requirements.sh
@@ -9,7 +9,7 @@ pip3 install matplotlib==2.0.2
 ##### Add files to the datasets/ folder on the live project #####
 
 # A list of files in datasets/ separated by space, needs to be manually updated
-FILES="global_temperature.csv run_code_cell_image.png"
+FILES="global_temperature.csv run_code_cell_image.png project_image.png"
 # The URL where the assets of the project lives
 ASSETURL=https://s3.amazonaws.com/assets.datacamp.com/production/project_33/datasets
 # Where the datasets will be compied on the Project image.
